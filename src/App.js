dd5036449ae5cfd4cd32d5999863d7ee533819e2
Samarth Goyal <samarth_niku@yahoo.co.in>
2020-09-06 15:09:30 +0530

    Add header with brand logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignupForm from './SignupForm';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <SignupForm />
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Header.css"
+
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <img class ="logo" src="logo192.png" />
+            </div>
+        );
+    }
+}
+
+export default Header;
